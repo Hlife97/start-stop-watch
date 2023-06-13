9a9a169014c9235eb5388e9e586ff7c6c215a123
Hlife97 <hidayet.hidayetov@gmail.com>
2023-06-13 22:39:07 +0400

    First Upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0)
+  const [changeMode, setChangeMode] = useState(false)
+
+  const handleChangeMode = () => {
+    setChangeMode(!changeMode)
+  };
+
+  useEffect(() => {
+    if (changeMode) {
+      const intervalId = setInterval(() => {
+        setCount((prevCount) => prevCount + 1);
+      }, 1000);
+
+      return () => {
+        clearInterval(intervalId);
+      };
+    }
+  }, [changeMode]);
+
+  const handleReset = () => {
+    setCount(0)
+    setChangeMode(true)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={handleChangeMode}>{changeMode ? 'Stop' : 'Start'}</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   );
 }
